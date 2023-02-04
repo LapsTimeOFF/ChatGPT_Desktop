fff3d4cf7ce9b17b139a021b28448296bd77469b
Quentin <39061148+LapsTimeOFF@users.noreply.github.com>
2023-02-04 20:46:36 +0100

    Push my code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import { Container, Typography } from "@mui/material";
+import "./App.scss";
+import ChatMessage from "./ChatMessage";
 
 function App() {
   return (
-    <Container>
-      <img src={"https://raw.githubusercontent.com/twitter/twemoji/master/assets/svg/1f95c.svg"} />
-      <Typography>Peanut</Typography>
-    </Container>
+    <div className="App">
+      <aside className="sidemenu">
+        <div className="side-menu-button">
+          <span>+</span>
+          New Chat
+        </div>
+      </aside>
+      <section className="chatbox">
+        <div className="chat-log">
+          <ChatMessage ai={false} message={'Peanut?'}/>
+          <ChatMessage ai={true} message={'PEANUT'}/>
+        </div>
+        <div className="chat-input-holder">
+          <textarea className="chat-input-textarea" rows={1}></textarea>
+        </div>
+      </section>
+    </div>
   );
 }
 
