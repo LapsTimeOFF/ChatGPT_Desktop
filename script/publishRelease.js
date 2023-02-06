eb0e304765cfad7228ff777bf9ed472fa0c4fc27
Quentin <39061148+LapsTimeOFF@users.noreply.github.com>
2023-02-06 20:34:56 +0100

    ci: Editing publishRelease.js

--- script/publishRelease.js
+++ script/publishRelease.js
@@ -1,4 +1,9 @@
-const { Octokit } = require("octokit");
+const {
+    Octokit
+} = require("octokit");
+const {
+    Writable
+} = require('stream');
 
 const conventionalChangelog = require('conventional-changelog');
 
@@ -8,16 +13,30 @@ const octokit = new Octokit({
     auth: process.env.GH_TOKEN
 })
 
-(async () => {
-    await octokit.request('POST /repos/LapsTimeOFF/ChatGPT_Destop/releases', {
-        owner: 'LapsTimeOFF',
-        repo: 'ChatGPT_Destop',
-        tag_name: `v${require('../package.json').version}`,
-        target_commitish: 'stable',
-        name: `v${require('../package.json').version}`,
-        body: conventionalChangelog({config}),
-        draft: true,
-        prerelease: false,
-        generate_release_notes: false
-    })
-})
+const publish = async () => {
+    var myStream = new Writable({
+        write(chunk, encoding, callback) {
+
+            octokit.request('POST /repos/LapsTimeOFF/ChatGPT_Desktop/releases', {
+                owner: 'LapsTimeOFF',
+                repo: 'ChatGPT_Desktop',
+                tag_name: `v${require('../package.json').version}`,
+                target_commitish: 'stable',
+                name: `v${require('../package.json').version}`,
+                body: chunk.toString(),
+                draft: true,
+                prerelease: false,
+                generate_release_notes: false
+            })
+
+
+            callback();
+        }
+    });
+    conventionalChangelog({
+        config
+    }).pipe(myStream)
+
+}
+
+publish();
